67fe7abe847021cf0153784ba13d0e6e53feaeac
Yin Zhan <yin.zhan.e@gmail.com>
2016-02-17 16:38:06 +0100

    Modification des actions du PoleEmploi

--- JadeProjet/src/core/PoleEmploi.java
+++ JadeProjet/src/core/PoleEmploi.java
@@ -4,10 +4,16 @@ import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 import jade.core.AID;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 public class PoleEmploi extends Agent {
 	/** Statut des employes. Surtout utile à des fins statistiques. */
@@ -50,6 +56,41 @@ public class PoleEmploi extends Agent {
 				String content = msg.getContent();
 				if (content.equals("Turn")){
 					System.out.println("PoleEmploi starting turn");
+				}
+				else if (content.startsWith("EmploiAccepte")){
+					
+				}
+				else if (content.startsWith("EmploiRefuse")){
+					
+				}
+				else if (msg.getConversationId().equals("PublierEmplois")){
+					try {
+						statutEmplois.put((Emploi)msg.getContentObject(), StatutEmploi.Disponible);
+					} catch (UnreadableException e) {
+						e.printStackTrace();
+					}
+				}
+				else {
+					DFAgentDescription[] dfds;
+					try {
+						dfds = DFService.decodeNotification(msg.getContent());
+						if (dfds.length > 0){
+							Iterator dfservice = dfds[0].getAllServices();
+							while (dfservice.hasNext()){
+								ServiceDescription sd = (ServiceDescription) dfservice.next();
+								if (sd.getType().startsWith("nivQualif")){
+									if (statutIndividus.containsKey(dfds[0].getName())){
+										statutIndividus.replace(dfds[0].getName(), StatutEmploye.Employe, StatutEmploye.Chomage);
+									}
+									else {
+										statutIndividus.put(dfds[0].getName(), StatutEmploye.Chomage);
+									}
+								}
+							}
+			            }
+					} catch (FIPAException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 			else {
